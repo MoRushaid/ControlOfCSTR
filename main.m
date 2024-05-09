e200a597acce6a49c71b5d649361a308502bf614
M Rushaid <53520288+MoRushaid@users.noreply.github.com>
2024-05-09 21:21:11 +0200

    Alignment Changes

--- main.m
+++ main.m
@@ -6,22 +6,22 @@ clear all;
 close all;
 
 %% Parameters
-p.Ea = 32400;             % Enthalpy [BTU/lbmol]
+p.Ea = 32400;               % Enthalpy [BTU/lbmol]
 p.k0 = 15e12;               % Constant [1/hr] 
-p.dH=45000;              % Change in Enthalpy [BTU/lbmol] 
-p.U = 75;                      % Heat Transfer Coefficient[BTU/hr-ft^2-oF] 
-p.rhocp = 53.2;           % [BTU/ft^3] 
-p.R = 1.987;                 % Specific Heat [BTU/lbmol-oF] 
-p.V = 750;                     % Volume [ft^3] 
-p.F = 3000;                  % Feed Rate [ft^3/hr] 
+p.dH = 45000;               % Change in Enthalpy [BTU/lbmol] 
+p.U  = 75;                  % Heat Transfer Coefficient[BTU/hr-ft^2-oF] 
+p.rhocp = 53.2;             % [BTU/ft^3] 
+p.R  = 1.987;               % Specific Heat [BTU/lbmol-oF] 
+p.V  = 750;                 % Volume [ft^3] 
+p.F  = 3000;                % Feed Rate [ft^3/hr] 
 p.Caf = 0.132;              % Final Concentration[lbmol/ft^3] 
-p.Tf = 60;                      % Final Temperature [oF] 
-p.A = 1221;                    % Area [ft^2] 
-p.Tj = 60;                      % Jacket Temperature [oF] 
+p.Tf = 60;                  % Final Temperature [oF] 
+p.A  = 1221;                % Area [ft^2] 
+p.Tj = 60;                  % Jacket Temperature [oF] 
 
 %% Initial Conditions
-x = [0.1 30];                 % x(1) = Ca   Concentration [lbmol/ft^3] 
-                                       % x(2) = T     Temperature [oF] 
+x = [0.1 30];               % x(1) = Ca   Concentration [lbmol/ft^3] 
+                            % x(2) = T     Temperature [oF] 
 tspan = [0 3];
 
 [t, x] = ode45(@reactor, tspan, x, [], p);
