1f43d8aa93a54b7ce5e2bf2818b96bbba25afab3
M Rushaid <53520288+MoRushaid@users.noreply.github.com>
2024-05-10 15:20:12 +0200

    Add files via upload

---
+++ reactor_sfcn.m
@@ -0,0 +1,48 @@
+function [sys,x0] = reactor_sfcn(t, x, u, flag, Cinit, Tinit)
+%
+%  Simulink interface to reactor_simu.m     
+%
+% Input arguments :    t    - time in [min].
+%                      x    -  states
+%                      u    -  inputs
+%                      flag -  indicator  of the Simulink request 
+%
+% Output arguments :  
+%            sys   - results requested by Simulink, varies for each flag#
+%            x0    -  initial conditions
+%--------------------------------------------------------------
+% depending on flag # , sys holds different information :
+%
+% when flag is 1, sys contains state derivatives,
+% when flag is 3 sys contains outputs; 
+%            y(1)    - ?
+%            y(2)    - ?
+% when flag is 0, sys is initialized 
+% Initialize :  define initial conditions,  X0
+%               define the system in terms of number of states, inputs etc.
+%             e.g. sys = [ a, b, c, d, e, f];
+%            1st array,a :  number of continuous states
+%            2nd array,b :  number of discrete states
+%            3rd array,c :  number of outputs
+%            4th array,d :  number of inputs
+%            5th array,e :  flag for direct feedthrough
+%            6th array,f :  the number of sample times
+            
+if abs(flag) == 1
+  % Return state derivatives.
+  sys =  reactor_simu(t, x, u);
+
+elseif abs(flag) == 3
+  % Return system outputs.
+  sys = x;
+
+elseif flag == 0
+  % Initialize the system
+  x0 = [Cinit Tinit] ;
+  sys = [2, 0, 2, 1, 0, 1];
+
+else
+  sys = [];  
+end
+  
+   
---
+++ reactor_simu.m
@@ -0,0 +1,28 @@
+function dx = reactor_simu(~,x,u)
+% Define states 
+Ca = x(1);
+T = x(2);
+
+% Inputs
+Tj  = u(1);
+
+% Parameters
+Ea = 32400;             % Enthalpy [BTU/lbmol]
+k0 = 15e12;               % Constant [1/hr] 
+dH=45000;              % Change in Enthalpy [BTU/lbmol] 
+U = 75;                      % Heat Transfer Coefficient[BTU/hr-ft^2-oF] 
+rhocp = 53.2;           % [BTU/ft^3] 
+R = 1.987;                 % Specific Heat [BTU/lbmol-oF] 
+V = 750;                     % Volume [ft^3] 
+F = 3000;                  % Feed Rate [ft^3/hr] 
+Caf = 0.132;              % Final Concentration[lbmol/ft^3] 
+Tf = 60;                      % Final Temperature [oF] 
+A = 1221;                    % Area [ft^2] 
+
+% Equations 
+ra = k0 * exp(-Ea / (R * (T + 460))) * Ca; 
+dCa = (F / V) * (Caf - Ca) - ra; 
+dT = (F / V) * (Tf - T) - (dH) / (rhocp) * ra - (U * A) / (rhocp * V) * (T - Tj); 
+dx = [dCa; dT]; 
+
+
